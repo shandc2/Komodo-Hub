b6e211e39051e8a2652612e82a7cd5db8e145cdf
William Bell <epicugric@gmail.com>
2026-02-05 17:07:38 +0000

    add homepage

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from routes.homepage import home_page
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='')
+
+app.register_blueprint(home_page)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=3000, debug=True)
--- routes/example/example.py
+++ routes/example/example.py
@@ -1,9 +1,7 @@
 from flask import render_template, request, Blueprint
-from database import db
 
-home_page = Blueprint('example', __name__, url_prefix='/example')
+example_page = Blueprint('example', __name__, url_prefix='/example')
 
 @home_page.route('/')
 def home():
-    user = db.get_user_by_token(request.cookies.get('token'))
-    return render_template('example/example.jinja', user=user)
+    return render_template('example/example.jinja')
