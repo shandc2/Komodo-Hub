2a37ed0c88304ff672cdf27f2282faf5a5bb110e
TussockyJoker <cheshand@hotmail.co.uk>
2026-02-11 13:06:10 +0000

    Updated CSS and homepage

--- routes/homepage.py
+++ routes/homepage.py
@@ -1,9 +1,10 @@
-from flask import render_template, request, Blueprint
+from flask import render_template, Blueprint
 from routes.donation.donation import donation_page
 
-home_page = Blueprint('home', __name__, url_prefix='/')
+home_page = Blueprint("home", __name__, url_prefix="/")
 home_page.register_blueprint(donation_page)
 
-@home_page.route('/')
+
+@home_page.route("/")
 def home():
-    return render_template('home/home.jinja')
+    return render_template("home/home.jinja")
