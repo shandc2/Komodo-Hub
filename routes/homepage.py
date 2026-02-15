5cf4f2ac5b03ccaa48272e4c5549e6a98ee0c777
TussockyJoker <cheshand@hotmail.co.uk>
2026-02-15 21:53:01 +0000

    Added error page

--- routes/homepage.py
+++ routes/homepage.py
@@ -1,10 +1,12 @@
 from flask import render_template, Blueprint
 from routes.donation.donation import donation_page
 from routes.species_info.species_info import species_info_page
+from routes.error.error import error_page
 
 home_page = Blueprint("home", __name__, url_prefix="/")
 home_page.register_blueprint(donation_page)
 home_page.register_blueprint(species_info_page)
+home_page.register_blueprint(error_page)
 
 
 @home_page.route("/")
