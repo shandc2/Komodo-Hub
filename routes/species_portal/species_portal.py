3035dd03d583a3e42d15d1146ccae9140050870a
TussockyJoker <cheshand@hotmail.co.uk>
2026-02-17 22:10:50 +0000

    species portal functional

--- routes/species_portal/species_portal.py
+++ routes/species_portal/species_portal.py
@@ -1,4 +1,6 @@
 from flask import render_template, Blueprint, request
+from utilities.add_species import add_species
+import os
 
 page = Blueprint("species_portal", __name__, url_prefix="/species_portal")
 
@@ -15,10 +17,16 @@ def add_species_to_database():
         latin_name = request.form["latin_name"]
         main_text = request.form["main_text"]
         species_image = request.files["species_image"]
+        species_image.save(f"static/images/species_database/{eng_name}.jpg")
+        add_species(eng_name, latin_name, main_text)
+        
         return render_template(
             "species_portal/species_portal_success.jinja",
             eng_name = eng_name,
             )
-    except:
-        return render_template("species_portal/species_portal_failed.jinja")
-    # return f"{eng_name}, {latin_name}, {main_text}"
+    except Exception as error_information:
+        return render_template(
+            "species_portal/species_portal_failed.jinja",
+            error_information=error_information
+            )
+        
