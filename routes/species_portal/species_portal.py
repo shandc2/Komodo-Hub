56707ce377e7ef0dfa073f8ddc3c55ed6f1ad695
TussockyJoker <cheshand@hotmail.co.uk>
2026-02-17 21:08:57 +0000

    fixed sql threading, added success/failure pages to species portal

--- routes/species_portal/species_portal.py
+++ routes/species_portal/species_portal.py
@@ -1,7 +1,24 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 
-page = Blueprint('species_portal', __name__, url_prefix='/species_portal')
+page = Blueprint("species_portal", __name__, url_prefix="/species_portal")
 
-@page.route('/')
+
+@page.route("/")
 def portal():
-    return render_template('species_portal/species_portal.jinja')
+    return render_template("species_portal/species_portal.jinja")
+
+
+@page.route("/", methods=["POST"])
+def add_species_to_database():
+    try:
+        eng_name = request.form["eng_name"]
+        latin_name = request.form["latin_name"]
+        main_text = request.form["main_text"]
+        species_image = request.files["species_image"]
+        return render_template(
+            "species_portal/species_portal_success.jinja",
+            eng_name = eng_name,
+            )
+    except:
+        return render_template("species_portal/species_portal_failed.jinja")
+    # return f"{eng_name}, {latin_name}, {main_text}"
