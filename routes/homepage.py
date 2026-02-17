f1a79aa13627a2f175ed71f271810335e06bf1b6
TussockyJoker <cheshand@hotmail.co.uk>
2026-02-17 15:37:38 +0000

    Added species portal page

--- routes/homepage.py
+++ routes/homepage.py
@@ -2,11 +2,14 @@ from flask import render_template, Blueprint
 from routes.donation.donation import donation_page
 from routes.species_info.species_info import species_info_page
 from routes.error.error import error_page
+from routes.species_portal.species_portal import species_portal
 
 home_page = Blueprint("home", __name__, url_prefix="/")
 home_page.register_blueprint(donation_page)
 home_page.register_blueprint(species_info_page)
 home_page.register_blueprint(error_page)
+home_page.register_blueprint(species_portal)
+
 
 
 @home_page.route("/")
