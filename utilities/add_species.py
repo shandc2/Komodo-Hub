0513d9fa14bf58384f73c5d88b4fa2d4e4c3a908
TussockyJoker <cheshand@hotmail.co.uk>
2026-02-18 12:29:10 +0000

    Attempted to add drop-down menu to navbar

--- utilities/add_species.py
+++ utilities/add_species.py
@@ -2,22 +2,22 @@ import sqlite3
 from datetime import datetime
 
 
-def database_connect():
-    database = sqlite3.connect("database/species.db", check_same_thread=False)
-    database.execute(
-        """
-    CREATE TABLE IF NOT EXISTS species (
-    species_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    species_english TEXT,
-    species_latin TEXT,
-    body_text TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )
-    """
-    )
-    database.commit()
 
-    print("Database initialized.")
+database = sqlite3.connect("database/species.db", check_same_thread=False)
+database.execute(
+    """
+CREATE TABLE IF NOT EXISTS species (
+species_id INTEGER PRIMARY KEY AUTOINCREMENT,
+species_english TEXT,
+species_latin TEXT,
+body_text TEXT,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+"""
+)
+database.commit()
+
+print("Database initialized.")
 
 
 def add_species(english, latin, body):
