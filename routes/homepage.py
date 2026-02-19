7932cc2c012676454abf65903b24c621675cf141
TussockyJoker <cheshand@hotmail.co.uk>
2026-02-19 15:45:04 +0000

    Updated homepage.py so that config.ini can be changed without restaring server, updated css

--- routes/homepage.py
+++ routes/homepage.py
@@ -2,19 +2,17 @@ from flask import render_template, Blueprint
 import configparser
 
 config = configparser.ConfigParser()
-config.read('config.ini')
-
-species1 = config['homepage']['featured_species_1']
-species2 = config['homepage']['featured_species_2']
-species3 = config['homepage']['featured_species_3']
-
-print(f'Featured species: {species1}, {species2}, {species3}')
 
 page = Blueprint("home", __name__, url_prefix="/")
 
 
 @page.route("/")
 def home():
+    config.read('config.ini')
+    species1 = config['homepage']['featured_species_1']
+    species2 = config['homepage']['featured_species_2']
+    species3 = config['homepage']['featured_species_3']
+    print(f'Featured species: {species1}, {species2}, {species3}')
     return render_template(
         "home/home.jinja",
         species1=species1,
