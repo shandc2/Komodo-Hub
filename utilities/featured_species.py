73d73e9b698f19f4d21300631bad2c05bce463d8
TussockyJoker <cheshand@hotmail.co.uk>
2026-03-03 22:47:37 +0000

    updated config.ini, removed unnecessary lines from species.jinja

--- utilities/featured_species.py
+++ utilities/featured_species.py
@@ -5,7 +5,7 @@ from database.db_commands import get_species_by_name
 def featured_species_function(config_file):
     config = configparser.ConfigParser()
     config.read(config_file)
-    featured_species_list = list(dict(config["homepage"]).values())
+    featured_species_list = list(dict(config["featured_species"]).values())
     featured_species = []
     for species in featured_species_list:
         featured_species.append(get_species_by_name(species))
